e94486e3220e56acd073a1b99138902b337ec1ee
Maja Solberg Petterson <majasp@uia.no>
2024-11-11 11:03:35 +0100

    Fikset Min side og fjernet Home knappen i menyen.

---
+++ Kartverket/Models/MinSideViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Kartverket.Data;
+
+namespace Kartverket.Models
+{
+    public class MinSideViewModel
+    {
+        public UserData UserData { get; set; }
+        public List<GeoChange> GeoChange { get; set; }
+    }
+}
