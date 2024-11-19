0d7d0d0f5001579999ecfac3dd883220ef166a7a
Odalopheim <odalo@uia.no>
2024-11-19 20:19:57 +0100

    funk

--- Kartverket/Models/GeoChangeDetailsViewModel.cs
+++ Kartverket/Models/GeoChangeDetailsViewModel.cs
@@ -6,5 +6,7 @@ namespace Kartverket.ViewModels
     {
         public GeoChange GeoChange { get; set; }
         public string UserName { get; set; }
+
+        public string Status { get; set; }
     }
 }
